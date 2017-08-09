95590b00be8bf8e9744012512c3a93c5a1fb492d
David Antognoli <david.antognoli@gmail.com>
2017-08-09 14:14:43 -0500

    initial project added

---
+++ Assets/HttpClientTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HttpClientTest : MonoBehaviour 
+{
+
+    void Start()
+    {
+        // System.Net.ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => { return true; };
+
+        Debug.Log("Start time: " + Time.time);
+        Test2();
+        //var message = Test1();
+        //Debug.Log(message.Result.Content.Headers.ToString());
+
+    }
+
+    private Task<HttpResponseMessage> Test1()
+    {
+        var httpClient = new HttpClient();
+
+        var t = httpClient.GetAsync("http://stackoverflow.com", HttpCompletionOption.ResponseHeadersRead);
+
+        return t;
+    }
+
+    private void Test2()
+    {
+        var httpClient = new HttpClient();
+
+        httpClient.GetStringAsync("http://www.microsoft.com").ContinueWith(t => Debug.Log(t));
+    }
+}
